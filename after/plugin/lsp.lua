68a9f2d499c880cd0eb843c02ceb0c6186a5c22d
Jace <jacedenny@gmail.com>
2023-05-15 21:08:23 +1000

    Remove depricated rust hover

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -101,7 +101,6 @@ local rust_tools = require('rust-tools')
 rust_tools.setup({
     tools = {
         autoSetHints = true,
-        hover_with_actions = true,
         inlay_hints = {
             show_parameter_hints = true
         },
