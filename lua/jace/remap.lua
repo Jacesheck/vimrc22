5fe8b2d694f7b5880476cf55622486258ddff97d
Jace <jacedenny@gmail.com>
2023-07-23 20:48:33 +1000

    Use correct clipboard on linux

--- lua/jace/remap.lua
+++ lua/jace/remap.lua
@@ -13,7 +13,6 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
--- Copy
 vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>Y", "\"+Y")
