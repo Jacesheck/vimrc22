8ed5c5768cd9c21c289b4c81c53eb3e449fee3cc
Jace Denny <j.denny@ascentvision.com.au>
2023-08-25 07:32:17 +1000

    Fix telescope references frame width

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -7,3 +7,29 @@ vim.keymap.set('n', '<C-p>f', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
+
+require('telescope').setup{
+  defaults = {
+    -- Default configuration for telescope goes here:
+    -- config_key = value,
+  },
+  pickers = {
+    -- Default configuration for builtin pickers goes here:
+    -- picker_name = {
+    --   picker_config_key = value,
+    --   ...
+    -- }
+    lsp_references = {
+        fname_width = 60,
+    }
+    -- Now the picker_config_key will be applied every time you call this
+    -- builtin picker
+  },
+  extensions = {
+    -- Your extension configuration goes here:
+    -- extension_name = {
+    --   extension_config_key = value,
+    -- }
+    -- please take a look at the readme of the extension you want to configure
+  }
+}
