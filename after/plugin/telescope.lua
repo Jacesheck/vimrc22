3a28b03a5ae1532b45bb4b71ec03bbecdd627aa0
Jace Denny <j.denny@ascentvision.com.au>
2024-01-03 15:03:45 +1100

    Keymap updates

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -4,9 +4,6 @@ vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>pg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>pr', builtin.lsp_references, {})
 vim.keymap.set('n', '<C-p>f', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function()
-    builtin.grep_string({ search = vim.fn.input("Grep > ") });
-end)
 
 require('telescope').setup{
   defaults = {
--- lua/jace/packer.lua
+++ lua/jace/packer.lua
@@ -14,8 +14,9 @@ return require('packer').startup(function(use)
 
     -- Telescope
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.0',
-        -- or                            , branch = '0.1.x',
+        'nvim-telescope/telescope.nvim',
+        --                           tag = '0.1.0',
+                                     branch = '0.1.x',
         requires = { {'nvim-lua/plenary.nvim'} }
     }
 
