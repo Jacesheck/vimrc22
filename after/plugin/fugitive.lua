e392fed7f97f9c469c164e42167ffd99801757b4
Jace <jacedenny@gmail.com>
2024-03-02 21:21:29 +1100

    feat(fugitive): add log and blame keymaps

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,2 +1,4 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 vim.keymap.set("n", "<leader>gb", function () vim.cmd.Git('branch') end)
+vim.keymap.set("n", "<leader>gl", function () vim.cmd.Git('log --oneline --decorate=full') end)
+vim.keymap.set("n", "<leader>gm", function () vim.cmd.Git('blame') end)
