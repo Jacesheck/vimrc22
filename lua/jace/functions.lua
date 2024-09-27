26809a02cf88d4eb95e1093ca3e7781cb07735fd
Jace <jacedenny@gmail.com>
2024-09-27 22:01:00 +1000

    fix(case function): non-lsp words

--- lua/jace/functions.lua
+++ lua/jace/functions.lua
@@ -9,11 +9,25 @@ local function switch_case()
             client_attached = true
         end
     end
+
+    local is_lsp_word = false
+    if client_attached then
+        vim.lsp.buf_request(vim.api.nvim_get_current_buf(),
+            'textDocumentHighlight',
+            vim.lsp.util.make_position_params(),
+            function(err, result, _, _)
+                if not err and result and #result > 0 then
+                    is_lsp_word = true
+                end
+            end
+        )
+    end
+
     -- Detect camelCase
     if word:find('[a-z][A-Z]') then
         -- Convert camelCase to snake_case
         local snake_case_word = word:gsub('([a-z])([A-Z])', '%1_%2'):lower()
-        if client_attached then
+        if is_lsp_word then
             vim.lsp.buf.rename(snake_case_word)
         else
             vim.api.nvim_buf_set_text(0, line - 1, word_start, line - 1, word_start + #word, { snake_case_word })
@@ -22,7 +36,7 @@ local function switch_case()
     elseif word:find('_[a-z]') then
         -- Convert snake_case to camelCase
         local camel_case_word = word:gsub('(_)([a-z])', function(_, l) return l:upper() end)
-        if client_attached then
+        if is_lsp_word then
             vim.lsp.buf.rename(camel_case_word)
         else
             vim.api.nvim_buf_set_text(0, line - 1, word_start, line - 1, word_start + #word, { camel_case_word })
