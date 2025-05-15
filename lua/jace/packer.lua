a17a75674fc4a7d0b99fbed0284a34812450d620
Jace Denny <j.denny@ascentvision.com.au>
2025-05-15 10:05:17 +1000

    add back tree-sitter-context

--- lua/jace/packer.lua
+++ lua/jace/packer.lua
@@ -22,6 +22,7 @@ return require('packer').startup(function(use)
 
     -- Treesitter
     use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+    use('nvim-treesitter/nvim-treesitter-context')
     use('mbbill/undotree')
 
     use {
