3fd5f2fabb99814a0eb6589e69e8e7748cdf786a
Jace Denny <j.denny@ascentvision.com.au>
2025-05-16 19:17:59 +1000

    fix virtual text and lua

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -13,40 +13,35 @@ local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
 require('mason').setup({})
 require('mason-lspconfig').setup({
     ensure_installed = {'lua_ls', 'clangd'},
-    handlers = {
-        function(server_name)
-            vim.lsp.config(server_name, {
-                capabilities = lsp_capabilities,
-                on_attach = on_attach,
-            })
-        end,
-        lua_ls = function()
-            vim.lsp.config('lua_ls', {
-                on_attach = on_attach,
-                capabilities = lsp_capabilities,
-                settings = {
-                    Lua = {
-                        runtime = {
-                            version = 'LuaJIT'
-                        },
-                        diagnostics = {
-                            globals = {'vim'},
-                        },
-                        workspace = {
-                            library = {
-                                vim.env.VIMRUNTIME,
-                            }
-                        }
-                    }
-                }
-            })
-        end,
-    }
 })
 
 vim.api.nvim_create_autocmd('LspAttach', {
     group = vim.api.nvim_create_augroup('user_lsp_attach', {clear = true}),
     callback = on_attach
+})
+
+vim.diagnostic.config({
+    virtual_text = true,
+})
+
+vim.lsp.config('lua_ls', {
+    on_attach = function() print("lua_ls") on_attach() end,
+    capabilities = lsp_capabilities,
+    settings = {
+        Lua = {
+            runtime = {
+                version = 'LuaJIT'
+            },
+            diagnostics = {
+                globals = {'vim', 'gc', 'ui', 'tasksuite'},
+            },
+            workspace = {
+                library = {
+                    vim.env.VIMRUNTIME,
+                }
+            }
+        }
+    }
 })
 
 local cmp = require('cmp')
