763cbda7fd3f353a4947d1a0d7e15e0d7aaa9354
Jace <jacedenny@gmail.com>
2025-05-24 13:47:39 +1000

    gitsigns fixed upstream

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -9,6 +9,5 @@ require('gitsigns').setup{
         map('n', '<leader>hp', "<cmd>Gitsigns preview_hunk<CR>")
         map('n', '<leader>hn', "<cmd>Gitsigns next_hunk<CR>")
         map('n', '<leader>hr', "<cmd>Gitsigns reset_hunk<CR>")
-        if vim.bo[bufnr].filetype == "netrw" then return false end
     end
 }
