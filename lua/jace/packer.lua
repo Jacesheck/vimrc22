5b00f1ad424ef441a270490bab6309a3aa33f8b6
Jace Denny <j.denny@ascentvision.com.au>
2026-04-10 14:57:39 +1000

    fix(telescope)

--- lua/jace/packer.lua
+++ lua/jace/packer.lua
@@ -15,8 +15,6 @@ return require('packer').startup(function(use)
     -- Telescope
     use {
         'nvim-telescope/telescope.nvim',
-        --                           tag = '0.1.0',
-                                     branch = '0.1.x',
         requires = { {'nvim-lua/plenary.nvim'} }
     }
 
